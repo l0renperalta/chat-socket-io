3003f9c2f537947dd4652dd71500f38882dc259a
loren <stinpc@hotmail.com>
2023-01-25 20:06:55 -0500

    chat finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,16 +31,18 @@ function App() {
    }, [messages]);
 
    return (
-      <div className="App">
-         <form onSubmit={handleSubmit}>
-            <input type="text" onChange={(e) => setMessage(e.target.value)} value={message} />
-            <button>send</button>
+      <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+         <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+            <h1 className="chat-2x1 font-bold my-2">Whasap</h1>
+            <input type="text" onChange={(e) => setMessage(e.target.value)} value={message} className="border-2 border-zinc-500 p-2 text-black w-full" />
+            <ul className="h-80 overflow-y-auto">
+               {messages.map((e, index) => (
+                  <li key={index} className={`my-2 p-2 text-sm rounded-md table ${e.from === 'Me' ? 'bg-sky-700 ml-auto' : 'bg-indigo-500'}`}>
+                     {e.from}: {e.message}
+                  </li>
+               ))}
+            </ul>
          </form>
-         {messages.map((e, index) => (
-            <div key={index}>
-               {e.from}: {e.message}
-            </div>
-         ))}
       </div>
    );
 }
